040284d9178112f798db3702a0c1a568be655396
Matt Harvey <harveym@gmail.com>
2017-11-17 16:03:04 -0800

    Use binary numpy saving/loading so we dont need pandas

--- train.py
+++ train.py
@@ -82,9 +82,9 @@ def train(data_type, seq_length, model, saved_model=None,
 def main():
     """These are the main training settings. Set each before running
     this file."""
-    model = 'lrcn'  # see `models.py` for more
+    model = 'lstm'  # see `models.py` for more
     saved_model = None  # None or weights file
-    class_limit = 10  # int, can be 1-101 or None
+    class_limit = 2  # int, can be 1-101 or None
     seq_length = 40
     load_to_memory = False  # pre-load the sequences into memory
     batch_size = 32
