366604087b089ad1b6b52b8782708cc840c1ab50
Sam Wemyss <samuel.wemyss@gmail.com>
2018-10-02 09:26:10 +1000

    fix py2 to py3

--- metrics.py
+++ metrics.py
@@ -12,11 +12,14 @@ class Metrics(Callback):
     def on_epoch_end(self, epoch, logs={}):
         val_predict = (np.asarray(self.model.predict(self.model.validation_data[0]))).round()
         val_targ = self.model.validation_data[1]
+
         _val_f1 = f1_score(val_targ, val_predict)
         _val_recall = recall_score(val_targ, val_predict)
         _val_precision = precision_score(val_targ, val_predict)
+
         self.val_f1s.append(_val_f1)
         self.val_recalls.append(_val_recall)
         self.val_precisions.append(_val_precision)
-        print " — val_f1: %f — val_precision: %f — val_recall %f" %(_val_f1, _val_precision, _val_recall)
+
+        print(" — val_f1: %f — val_precision: %f — val_recall %f" % (_val_f1, _val_precision, _val_recall))
         return
