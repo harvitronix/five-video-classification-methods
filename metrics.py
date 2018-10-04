65289b50e830ff113b93ff521b38ad2841ffaf93
Sam Wemyss <samuel.wemyss@gmail.com>
2018-10-04 12:29:07 +1000

    fix functions naming in metrics.py

--- metrics.py
+++ metrics.py
@@ -11,19 +11,19 @@ def specificity(y_true, y_pred):
     possible_negatives = np.sum(np.round(np.clip(1-y_true, 0, 1)))
     return true_negatives / (possible_negatives + np.epsilon())
 
-def recall(y_true, y_pred):
+def _recall(y_true, y_pred):
         true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
         possible_positives = np.sum(np.round(np.clip(y_true, 0, 1)))
         recall = true_positives / (possible_positives + np.epsilon())
         return recall
 
-def precision(y_true, y_pred):
+def _precision(y_true, y_pred):
     true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
     predicted_positives = np.sum(np.round(np.clip(y_pred, 0, 1)))
     precision = true_positives / (predicted_positives + np.epsilon())
     return precision
 
 def f1(y_true, y_pred):
-    precision = precision(y_true, y_pred)
-    recall = recall(y_true, y_pred)
+    precision = _precision(y_true, y_pred)
+    recall = _recall(y_true, y_pred)
     return 2 * ((precision*recall)/(precision+recall)) 
