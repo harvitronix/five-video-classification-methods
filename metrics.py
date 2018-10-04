bbea05bcad74f263962e76841a17d6054a46a42d
Sam Wemyss <samuel.wemyss@gmail.com>
2018-10-04 12:39:07 +1000

    tmp

--- metrics.py
+++ metrics.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from sklearn.metrics import f1_score, precision_score, recall_score
 
 def sensitivity(y_true, y_pred):
     true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
@@ -11,19 +11,19 @@ def specificity(y_true, y_pred):
     possible_negatives = np.sum(np.round(np.clip(1-y_true, 0, 1)))
     return true_negatives / (possible_negatives + np.epsilon())
 
-def _recall(y_true, y_pred):
-        true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
-        possible_positives = np.sum(np.round(np.clip(y_true, 0, 1)))
-        recall = true_positives / (possible_positives + np.epsilon())
-        return recall
+# def _recall(y_true, y_pred):
+#         true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
+#         possible_positives = np.sum(np.round(np.clip(y_true, 0, 1)))
+#         recall = true_positives / (possible_positives + np.epsilon())
+#         return recall
 
-def _precision(y_true, y_pred):
-    true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = np.sum(np.round(np.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + np.epsilon())
-    return precision
+# def _precision(y_true, y_pred):
+#     true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
+#     predicted_positives = np.sum(np.round(np.clip(y_pred, 0, 1)))
+#     precision = true_positives / (predicted_positives + np.epsilon())
+#     return precision
 
 def f1(y_true, y_pred):
-    precision = _precision(y_true, y_pred)
-    recall = _recall(y_true, y_pred)
-    return 2 * ((precision*recall)/(precision+recall)) 
+    # precision = _precision(y_true, y_pred)
+    # recall = _recall(y_true, y_pred)
+    return f1_score(y_true, y_pred)
