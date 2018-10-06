689d61aaf5136b1e978e5bf42043fcd15fbfc560
Sam Wemyss <samuel.wemyss@gmail.com>
2018-10-06 14:28:30 +1000

    :trollface:

--- metrics.py
+++ metrics.py
@@ -1,29 +1,19 @@
-import numpy as np
-from sklearn.metrics import f1_score, precision_score, recall_score
+import functools
+from keras import backend as K
+import tensorflow as tf
 
-def sensitivity(y_true, y_pred):
-    true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
-    possible_positives = np.sum(np.round(np.clip(y_true, 0, 1)))
-    return true_positives / (possible_positives + np.epsilon())
+def as_keras_metric(method):
+    @functools.wraps(method)
+    def wrapper(self, args, **kwargs):
+        """ Wrapper for turning tensorflow metrics into keras metrics """
+        value, update_op = method(self, args, **kwargs)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([update_op]):
+            value = tf.identity(value)
+        return value
+    return wrapper
 
-def specificity(y_true, y_pred):
-    true_negatives = np.sum(np.round(np.clip((1-y_true) * (1-y_pred), 0, 1)))
-    possible_negatives = np.sum(np.round(np.clip(1-y_true, 0, 1)))
-    return true_negatives / (possible_negatives + np.epsilon())
 
-# def _recall(y_true, y_pred):
-#         true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
-#         possible_positives = np.sum(np.round(np.clip(y_true, 0, 1)))
-#         recall = true_positives / (possible_positives + np.epsilon())
-#         return recall
-
-# def _precision(y_true, y_pred):
-#     true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
-#     predicted_positives = np.sum(np.round(np.clip(y_pred, 0, 1)))
-#     precision = true_positives / (predicted_positives + np.epsilon())
-#     return precision
-
-def f1(y_true, y_pred):
-    # precision = _precision(y_true, y_pred)
-    # recall = _recall(y_true, y_pred)
-    return f1_score(np.asarray(y_true), np.asarray(y_pred))
+precision = as_keras_metric(tf.metrics.precision)
+recall = as_keras_metric(tf.metrics.recall)
+f1_score = as_keras_metric(tf.contrib.metrics.f1_score)
